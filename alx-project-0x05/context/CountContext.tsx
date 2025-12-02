db69fb0c1c654bb41286f90a88844f41b64ecaad
Glory <gloryomoruyi00@gmail.com>
2025-12-02 06:03:16 +0100

    removed the commas

--- alx-project-0x05/context/CountContext.tsx
+++ alx-project-0x05/context/CountContext.tsx
@@ -1,8 +1,8 @@
 import { createContext, useContext,  useState, ReactNode } from "react"
 
 interface CountContextProps {
-  count: number,
-  increment: () => void,
+  count: number;
+  increment: () => void;
   decrement: () => void;
 }
 
